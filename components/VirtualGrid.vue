ba958318555fed14b2d97daff085b885f5de41de
Warm Beer <mickvd99@gmail.com>
2025-07-27 12:06:55 +0200

    fix: update AvatarCard color scheme for consistency with global styles and improved readability

--- components/VirtualGrid.vue
+++ components/VirtualGrid.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="py-4 sm:px-2 md:px-8 overflow-x-hidden h-full w-full" ref="container">
-    <div class="mx-auto w-full h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 overflow-x-hidden sm:gap-3 divide-y sm:divide-y-0 divide-white/5">
+  <div class="md:py-4 sm:px-2 md:px-8 w-full" ref="container">
+    <div class="mx-auto w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 sm:gap-3 divide-y sm:divide-y-0 divide-white/5">
       <template v-for="(item, index) in visibleItems">
         <slot :item="item" :index="index"></slot>
       </template>
